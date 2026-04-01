891cfffe1925381955fb07a5f2e33df70a5d9766
Daniel <gdaniel998@gmail.com>
2026-04-01 14:38:39 -0400

    Fixed errors loading the file

--- src/main/java/com/sportsintel/SplashController.java
+++ src/main/java/com/sportsintel/SplashController.java
@@ -20,7 +20,7 @@ public class SplashController {
 
     @FXML
     public void initialize() {
-        Image image = new Image(Objects.requireNonNull(getClass().getResource("/resources/logo.png")).toExternalForm());
+        Image image = new Image(Objects.requireNonNull(getClass().getResource("/logo.png")).toExternalForm());
         logoImage.setImage(image);
 
         PauseTransition delay = new PauseTransition(Duration.seconds(2.5));
