50d7cd1c649fb864560975fc0937273ce3bb2ebf
Daniel <gdaniel998@gmail.com>
2026-04-03 16:52:20 -0400

    Created the main frame with adding comments in syles.css to show the sections

--- src/main/java/com/sportsintel/HomeController.java
+++ src/main/java/com/sportsintel/HomeController.java
@@ -13,15 +13,28 @@ public class HomeController {
     private ImageView navLogo;
 
     @FXML
+    private ImageView mainLogo;
+
+    @FXML
     private ComboBox<String> seasonTypeCombo;
 
     @FXML
     private ComboBox<String> locationCombo;
 
     @FXML
+    private ComboBox<String> sportCombo;
+
+    @FXML
+    private ComboBox<String> opponentCombo;
+
+    @FXML
+    private ComboBox<String> statisticCombo;
+
+    @FXML
     public void initialize() {
         Image image = new Image(Objects.requireNonNull(getClass().getResource("/newlogo.png")).toExternalForm());
         navLogo.setImage(image);
+        mainLogo.setImage(image);
 
         if (seasonTypeCombo != null) {
             seasonTypeCombo.getItems().addAll("Regular Season", "Playoffs", "Both");
@@ -31,6 +44,47 @@ public class HomeController {
         if (locationCombo != null) {
             locationCombo.getItems().addAll("Home", "Away", "Both");
             locationCombo.setValue("Both");
+        }
+
+        if (statisticCombo != null) {
+            statisticCombo.getItems().addAll("Points Per Game", "Assists Per Game", "Rebounds Per Game");
+        }
+
+        if (sportCombo != null) {
+            sportCombo.getItems().addAll("Basketball", "Baseball (Coming Soon)", "Football (Coming Soon)", "Soccer (Coming Soon)");
+        }
+
+        if (opponentCombo != null) {
+            opponentCombo.getItems().addAll("Atlanta Hawks",
+                    "Boston Celtics",
+                    "Brooklyn Nets",
+                    "Charlotte Hornets",
+                    "Chicago Bulls",
+                    "Cleveland Cavaliers",
+                    "Dallas Mavericks",
+                    "Denver Nuggets",
+                    "Detroit Pistons",
+                    "Golden State Warriors",
+                    "Houston Rockets",
+                    "Indiana Pacers",
+                    "Los Angeles Clippers",
+                    "Los Angeles Lakers",
+                    "Memphis Grizzlies",
+                    "Miami Heat",
+                    "Milwaukee Bucks",
+                    "Minnesota Timberwolves",
+                    "New Orleans Pelicans",
+                    "New York Knicks",
+                    "Oklahoma City Thunder",
+                    "Orlando Magic",
+                    "Philadelphia 76ers",
+                    "Phoenix Suns",
+                    "Portland Trail Blazers",
+                    "Sacramento Kings",
+                    "San Antonio Spurs",
+                    "Toronto Raptors",
+                    "Utah Jazz",
+                    "Washington Wizards");
         }
     }
 }
