3e18d7ad89bf5e1a8c6158267d9590ec3ac81413
Daniel <gdaniel998@gmail.com>
2026-04-03 12:22:22 -0400

    Created the filters section and changed background to baby blue

--- src/main/java/com/sportsintel/HomeController.java
+++ src/main/java/com/sportsintel/HomeController.java
@@ -1,6 +1,7 @@
 package com.sportsintel;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -12,8 +13,24 @@ public class HomeController {
     private ImageView navLogo;
 
     @FXML
+    private ComboBox<String> seasonTypeCombo;
+
+    @FXML
+    private ComboBox<String> locationCombo;
+
+    @FXML
     public void initialize() {
         Image image = new Image(Objects.requireNonNull(getClass().getResource("/newlogo.png")).toExternalForm());
         navLogo.setImage(image);
+
+        if (seasonTypeCombo != null) {
+            seasonTypeCombo.getItems().addAll("Regular Season", "Playoffs", "Both");
+            seasonTypeCombo.setValue("Both");
+        }
+
+        if (locationCombo != null) {
+            locationCombo.getItems().addAll("Home", "Away", "Both");
+            locationCombo.setValue("Both");
+        }
     }
 }
