5d9b29ff831f4eced0adc80aa1c709e47917f990
reyes <reyevj@farmingdale.edu>
2026-04-16 22:14:05 -0400

    added basic user info in User class

--- src/main/java/com/sportsintel/User.java
+++ src/main/java/com/sportsintel/User.java
@@ -1,4 +1,34 @@
 package com.sportsintel;
 
 public class User {
+    private String username;
+    private String first_name;
+    private String last_name;
+
+
+    public User(String username, String first_name, String last_name) {
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
